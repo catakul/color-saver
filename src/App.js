b04ac44f2ffb4c5e4ec97c4c045903d074d77a0f
Ralf Siewert (DCI) <ralf.siewert-freelancer@digitalcareerinstitute.org>
2022-06-27 00:44:05 +0200

    styles only

--- src/App.js
+++ src/App.js
@@ -1,31 +1,9 @@
 import "./App.css";
-import { ColorForm } from "./components/ColorForm";
-import { ColorBox } from "./components/ColorBox";
-
-const colors = [
-  {
-    id: 1,
-    code: "#123456",
-  },
-  {
-    id: 2,
-    code: "#456789",
-  },
-  {
-    id: 3,
-    code: "#156728",
-  },
-];
 
 function App() {
   return (
     <div className="App">
-      <ColorForm />
-      <div className="card-grid">
-        {colors.map((color) => {
-          return <ColorBox color={color.code} key={color.id} />;
-        })}
-      </div>
+      <h1>Color Saver Step 0</h1>
     </div>
   );
 }
