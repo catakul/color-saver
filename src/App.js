ec054e2f568441322a8d9916202d2fdcd846d066
Ralf Siewert (DCI) <ralf.siewert-freelancer@digitalcareerinstitute.org>
2022-06-27 02:11:35 +0200

    change h1 text

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { ColorBox } from "./components/ColorBox";
 function App() {
   return (
     <div className="App">
-      <h1>Color Saver Step 0</h1>
+      <h1>Color Saver Step 1</h1>
       <div className="card-grid">
         <ColorBox color="#f00" />
         <ColorBox color="#0f0" />
