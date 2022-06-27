9657b522cb8c35965bccb8d96b639507f9d786a8
Ralf Siewert (DCI) <ralf.siewert-freelancer@digitalcareerinstitute.org>
2022-06-28 01:08:11 +0200

    add branch description

--- src/App.js
+++ src/App.js
@@ -29,6 +29,7 @@ function App() {
   return (
     <div className="App">
       <h1>Color Saver Step 8</h1>
+      <small>Replace alert with auto-removing toast message</small>
       <ColorForm onSubmit={handleAddColor} />
       <div className="card-grid">
         {colors.map((color) => {
